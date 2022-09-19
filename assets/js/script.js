1978beafdd905c2e0647d57b1cb0ea5d6c241553
noreen <noreennelligan1@gmail.com>
2022-09-19 22:26:05 +0000

    add javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,32 @@
+const square = document.querySelectorAll('.square');
+const score = document.querySelector('.score');
+const catm0 = document.getElementById('.catm0');
+let score = 0;
+let lastSquare;
+let timeLeft = false;
 
 
 
+function timeRandom(min, max) {
+      return Math.floor(Math.random() * (max-min)+ min);
+}
 
+function square(square){
+      const index = Math.floor(Math.random() * square.length);
+      const square = square[index];
+}
+
+if (square === lastSquare){
+      return square(square);
+}
+
+lastSquare = square;
+return square;
+
+function catmappear() {((
+      const timeLeft = time (200, 450);
+      const square = square;
+      square.classList.add('upwards');
+      square.classList.add('up
+      setTimeleft))')
+}
