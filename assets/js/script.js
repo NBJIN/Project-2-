c6dff46c491d65af9e62cceb7c285b971f7968b6
noreen <noreennelligan1@gmail.com>
2022-09-19 08:56:02 +0000

    CSS Styling

--- assets/js/script.js
+++
@@ -1,6 +0,0 @@
-const clockValue =document.querySelector('#clock');
-const countdownMinute = new ScriptProcessorNode(60).getTime();
-function countdown() {
-    const minute = new Minute().getTime();
-    let difference 
-}
