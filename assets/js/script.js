b022f5d2b578fcc80d30a6f3191691a4934b0883
noreen <noreennelligan1@gmail.com>
2022-09-21 21:38:05 +0000

    addition of js code and css

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,8 +14,89 @@ let random;
 let setTimeout;
 let timer = 20;
 
+// Define 9 cats
+const cat1 = document.getElementsByClassName("catm0")[0];
+const cat2 = document.getElementsByClassName("catm1")[0];
+const cat3 = document.getElementsByClassName("catm2")[0];
+const cat4 = document.getElementsByClassName("catm3")[0];
+const cat5 = document.getElementsByClassName("catm4")[0];
+const cat6 = document.getElementsByClassName("catm5")[0];
+const cat7 = document.getElementsByClassName("catm6")[0];
+const cat8 = document.getElementsByClassName("catm7")[0];
+const cat9 = document.getElementsByClassName("catm8")[0];
 
+const startBtn = document.getElementsByClassName('buttonstart')[0];
 
+function start() {
+      //Stop showing play button
+      startBtn.style.display = 'none';
+      // Enter game
+      enterGame();
+      startTimer();
+}
+
+function startTimer() {
+      setInterval(function() {
+            timer--;
+            console.log(timer);
+      }, 1000);
+      }
+
+function enterGame() {
+      hideCats();
+      setInterval(function() {
+            displayCat();
+            if (timer === 0) {
+                  endGame();
+                  timer = -1;
+            } else {
+                  enterGame();
+            }
+      }, 2000);
+}
+
+function endGame() {
+      alert(`Game over, your time has run out, you have a score of ${scoreValue}`);
+}
+
+function hideCats() {
+      // Hide Cats
+      cat1.style.display = 'none';
+      cat2.style.display = 'none';
+      cat3.style.display = 'none';
+      cat4.style.display = 'none';
+      cat5.style.display = 'none';
+      cat6.style.display = 'none';
+      cat7.style.display = 'none';
+      cat8.style.display = 'none';
+      cat9.style.display = 'none';
+
+}
+
+function displayCat() {
+      const randomNumber = Math.floor(Math.random() *9);
+      if (randomNumber ===1) {
+            cat1.style.display = 'block';
+      } else if (randomNumber === 2) {
+            cat2.style.display = 'block';
+      } else if (randomNumber === 3) {
+            cat3.style.display = 'block';
+      } else if (randomNumber === 4) {
+            cat4.style.display = 'block';
+      } else if (randomNumber === 5) {
+            cat5.style.display = 'block';
+      } else if (randomNumber === 6) {
+            cat6.style.display = 'block';
+      } else if (randomNumber === 7) {
+           cat7.style.display = 'block';
+      } else if (randomNumber === 8) {
+            cat8.style.display = 'block';
+      } else  {
+            cat9.style.display = 'block';
+      }
+      }
+
+      /** 
 
 function start() {
       cat0[0].style.visibility = 'hidden';
