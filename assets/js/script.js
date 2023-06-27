bd7c11240c0172ee06440e9d79b95050e65b09e4
noreen <noreennelligancilms@gmail.com>
2023-06-27 22:50:31 +0000

    Revised functions in order to get timer to stop
    Revised functions in order to get timer to stop. Once ok is clicked
    on the alert at the end of the game the countdown timere will start
    again automatically so rviewing code to fix same.  in script.js file.
    Renamed reset button in html file and also updated its name in style.
    css file.

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,106 +9,144 @@ catImg.style.height = '80%';
 
 
 // const startButton = document.getElementById('startButton');
-const cat0 = document.getElementsByClassName('catm0');
+// const cat0 = document.getElementsByClassName('catm0');
 
 // const startBtn = document.getElementsByClassName('buttonstart')[0];
 const startButton = document.getElementById('startButton');
+const resetButton = document.getElementById('resetButton');
 const square = document.getElementsByClassName("square");
-const score = document.getElementsByClassName("score");
+const score = document.getElementsByClassName(".score .value");
 var timerRemaining = document.querySelector('.timeremaining .seconds');
+var catMoveInterval;
 
 startButton.addEventListener('click', start);
+resetButton.addEventListener('click', reset);
+catImg.addEventListener('click', incrementScore);
 
 
 function start() {
-      countdownInterval = setInterval(countdown, 1000);
-
-      displayCat();
-      setInterval(displayCat, 1000);
+  countdownInterval = setInterval(countdown, 1000);
+  displayCat();
+  catMoveInterval = setInterval(displayCat, 1000);
+  startButton.disabled = true;
 }
 
 
 function countdown() {
-      if (timer > 0) {
-      timer--;
-      timerRemaining.textContent = timer;
-      }
-
-      if (timer === 0) {
-            endGame();
-
-      }
+  if (timer > 0) {
+    timer--;
+    timerRemaining.textContent = timer;
 }
 
-
-// function to enter game 
-function enterGame() {
-      clearInterval(countdownInterval);
-      alert('The Game has now ended your score is: ' + currentScore);
+  if (timer === 0) {
+    endGame();
+  }
 }
 
-      var randomIndex = Math.floor(Math.random() * square.length);
-
-      square[randomIndex].appendChild(catImg);
-
-      catImg.addEventListener('click', incrementScore);
-
-// Once game is started the startTimer Functiion starts
-function startTimer() {
-      setInterval(() => {
-            timer --;
-            console.log(timer);
-
-            if(timer === 0) {
-                  gameOver();
-                  timer = -1;
-            }
-      }, 1000);
-      }
 
 // function that ends game once timer has finished
 function endGame() {
-      clearInterval(countdownInterval);
+  clearInterval(countdownInterval);
+  clearInterval(catMoveInterval);
+  catImg.removeEventListener('click', incrementScore);
+  // if (timer !== -1) {
+  alert('Time is up game has now ended your score is as follows: ' + currentScore);
+  resetGame();
+      
+  startButton.disabled = false;
+//   catImg.removeEventListener('click', incrementScore);
+  }
 
-      if (timer !== -1) {
-      alert('Time is up game has now ended your score is as follows: ' + currentScore);
 
-      timer = -1;
-      currentScore = 0;
-      timerRemaining.textContent = timer;
-      score[0].textContent = currentScore;
+// function to enter game 
+// function enterGame() {
+//   clearInterval(countdownInterval);
+//   alert('The Game has now ended your score is: ' + currentScore);
+// }
 
-      catImg.removeEventListener('click', incrementScore);
+// var randomIndex = Math.floor(Math.random() * square.length);
+//   square[randomIndex].appendChild(catImg);
+//   catImg.addEventListener('click', incrementScore);
+
+
+function resetGame() {
+  timer = 20;
+  currentScore = 0;
+  timerRemaining.textContent = timer;
+  score.textContent = currentScore;
 }
-}
-
-
-
-function catsHide() {
-       if (currentSquare !== null) {
-            const selectSquare = square[selectSquare];
-            targetSquare.innerHTML = '';
-            currentSquare = null;
-      }
-}
-
 
 // Display random cat
 function displayCat() {
-      for (var i = 0; i < square.length; i++) {
-            square[i].innerHTML = '';
+  for (var i = 0; i < square.length; i++) {
+      square[i].innerHTML = '';
       }
 
-      var randomIndex = Math.floor(Math.random() * square.length);
+  if (timer > 0) {
 
-      square[randomIndex].appendChild(catImg);
-
-      catImg.addEventListener('click', incrementScore);
+    var randomIndex = Math.floor(Math.random() * square.length);
+    square[randomIndex].appendChild(catImg);
+    square[randomIndex].addEventListener('click', incrementScore);
+  }
 }
 
-     function incrementScore() {
-            currentScore++;
-            score[0].textContent = currentScore;
-     }
+//     catImg.addEventListener('click', incrementScore);
+//   } else {
+//     catImg.removeEventListener('click', incrementScore);
+//  }
+// }
+
+
+function incrementScore() {
+   currentScore++;
+   score.textContent = currentScore;
+}
+
+
+function reset() {
+  resetGame();
+  clearInterval(countdownInterval);
+  clearInterval(catMoveInterval);
+  startButton.disabled = false; 
+}
      
    
+
+
+
+
+
+
+
+
+
+// Once game is started the startTimer Functiion starts
+
+// function startTimer() {
+//       setInterval(() => {
+//             timer --;
+//             console.log(timer);
+
+//             if(timer === 0) {
+//                   gameOver();
+//                   timer = -1;
+//             }
+//       }, 1000);
+//       }
+
+
+// }
+
+
+
+
+
+// function catsHide() {
+//        if (currentSquare !== null) {
+//             const selectSquare = square[selectSquare];
+//             targetSquare.innerHTML = '';
+//             currentSquare = null;
+//       }
+// }
+
+
